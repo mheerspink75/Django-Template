477963ca4edd7b28a60ddb7d59760a138b01c974
Matt Heerspink <mheerspink75@gmail.com>
2019-11-13 22:53:53 -0600

    updated url patterns

--- app1/urls.py
+++ app1/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from app1 import views
 
 urlpatterns = [
-  url(r'^admin/', admin.site.urls),
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
 ]
